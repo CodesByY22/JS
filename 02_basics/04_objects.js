75871843cb0df0bb24280b9cd8023755ce842fe3
CodesByY22 <yashkotadiya08@gmail.com>
2025-06-19 12:43:55 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,7 +26,63 @@ const obj1 = {1 : "a", 2 : "b" }
 const obj2 = {3 : "c", 4 : "d" }
 
 // const obj3 = {obj1,obj2}
-const obj3 = Object.assign(obj1,obj2) // this will merge obj1 and obj2 into obj1
-console.log(obj3);
+// const obj3 = Object.assign(obj1,obj2) // this will merge obj1 and obj2 into obj1
+// console.log(obj3);
 
+
+// const  obj3 = {...obj1,...obj2}
+// console.log(obj3)
+
+const users = [
+    {
+        id : 1,
+        emial: "y22@.com",  
+    },
+    {
+        id : 1,
+        emial: "y22@.com",  
+    },
+    {
+        id : 1,
+        emial: "y22@.com",  
+    },
+]
+
+// users[1].email 
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+const course = { 
+    courseName : "javascript",
+    price : 999,    
+    courseInstructor : "yash",
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course; // destructuring
+console.log(courseInstructor);
+
+
+const {courseInstructor : i} = course; // destructuring
+console.log(i);
+
+// {
+//     "name" :"yash",
+//     "coursename" : "javascript",
+//     "price" : 999,
+// }
+
+[
+    {},
+    {},
+    {}
+]
 
